d945de44d8c4ee9657eb71a692d4140288e77426
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-03-17 11:15:16 +0200

    Add files via upload

--- sensor.py
+++ sensor.py
@@ -1,14 +1,14 @@
 import Adafruit_DHT
 import time
+
 sensor = Adafruit_DHT.DHT22
 
 pin = 23
-
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 while True:
 	if humidity is not None and temperature is not None:	
 		print('Temp={0:1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-	time.sleep(.500);
+	else:
+		print('Failed to get reading. Try again!')
+	time.sleep(.500)
