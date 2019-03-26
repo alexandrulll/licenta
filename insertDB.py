f94f5babee43371c099abb39b570adfe7452d3ed
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-03-26 22:15:50 +0200

    Add files via upload

--- insertDB.py
+++ insertDB.py
@@ -13,7 +13,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 	if humidity is not None and temperature is not None:	
-		cur.execute("INSERT INTO dht11 (temperature, humidity)", (temperature, humidity))
+		cur.execute("INSERT INTO dht11 (temperature, humidity);", (temperature, humidity))
 		conn.commit()
 	else:
 		print('Failed to get reading. Try again!')
