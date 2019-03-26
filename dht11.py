345c9e7879690959779fa540a44c7039756d91ae
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-03-26 22:01:52 +0200

    Add files via upload

--- dht11.py
+++ dht11.py
@@ -9,7 +9,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 	if humidity is not None and temperature is not None:	
-		print(temperature, humidity)
+		print temperature, humidity
 	else:
 		print('Failed to get reading. Try again!')
 	time.sleep(.500)
