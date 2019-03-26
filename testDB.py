8f1d1c1f2186b6b1974eca78afad63477cb702c9
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-03-26 21:23:04 +0200

    Add files via upload

--- testDB.py
+++ testDB.py
@@ -3,7 +3,13 @@ import psycopg2
 conn = psycopg2.connect('dbname=test')
 cur = conn.cursor()
 
-cur.execute("""CREATE TABLE dht11(
+create_table_query = """CREATE TABLE dht11(
     id integer PRIMARY KEY,
     temperature text,
-    humidity text)""")
+    humidity text);"""
+
+cur.execute(create_table_query)
+	
+ connection.commit()
+ 
+ print("Table created successfully in PostgreSQL ")
