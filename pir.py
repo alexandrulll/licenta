101fdc73bbf9340e430af8b6fb96cbbe669272ab
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-04 18:27:24 +0300

    Add files via upload

---
+++ pir.py
@@ -0,0 +1,22 @@
+import RPI.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.OUT)
+
+try:
+	time.sleep(2)
+	
+	while True:
+		if GPIO.input(23):
+			GPIO.output(24, True)
+			time.sleep(0.5)
+			GPIO.output(24, False)
+			print("Motion Detected")
+			time.sleep(5)
+		time.sleep(0.1)
+		
+except:
+	GPIO.cleanup()
