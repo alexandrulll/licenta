61ec69f7ae448d401f17974e1bbf1dd2da7a625a
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-04 21:05:17 +0300

    Add files via upload

--- relay.py
+++ relay.py
@@ -6,12 +6,18 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(32, GPIO.OUT)
 GPIO.output(32, GPIO.HIGH)
 
+GPIO.setup(36, GPIO.OUT)
+GPIO.output(36, GPIO.HIGH)
+
 try:
 	GPIO.output(32, GPIO.LOW)
 	print('Relay 1 is ON')
 	time.sleep(2)
+	GPIO.output(36, GPIO.LOW)
+	print('Relay 2 is ON')
+	time.sleep(4)
 	GPIO.cleanup()
-	print('Relay 1 is OFF!!!')
+	print('Relay 1 and 2 are OFF!!!')
 	
 except KeyboardInterrupt:
 		print('QUIT')
