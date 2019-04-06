8ff6ee1aa56ae2138942d2d1a66050be72c8bc9c
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-06 14:52:01 +0300

    Add files via upload

--- tsl.py
+++ tsl.py
@@ -1,19 +1,22 @@
 import smbus
 import time
 
-bus = smbus.SMBus(1)
+while True:
 
-bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
-bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
+	bus = smbus.SMBus(1)
 
-time.sleep(0.5)
+	bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+	bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
 
-data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
-data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+	time.sleep(0.5)
 
-ch0 = data[1] * 256 + data[0]
-ch1 = data1[1] * 256 + data1[0]
+	data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+	data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
 
-print "Full Spectrum(IR + Visible) :%d lux" %ch0
-print "Infrared Value :%d lux" %ch1
-print "Visible Value :%d lux" %(ch0 - ch1)
+	ch0 = data[1] * 256 + data[0]
+	ch1 = data1[1] * 256 + data1[0]
+
+	print "-----------------------------------"
+	print "Full Spectrum(IR + Visible) :%d lux" %ch0
+	print "Infrared Value :%d lux" %ch1
+	print "Visible Value :%d lux" %(ch0 - ch1)
