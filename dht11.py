bbeeb3f327104fca569c1ff12c945a1805aba423
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-08 22:28:33 +0300

    Add files via upload

--- dht11.py
+++ dht11.py
@@ -1,6 +1,11 @@
 import Adafruit_DHT
 import time
 
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(38, GPIO.OUT)
+GPIO.output(38, GPIO.HIGH)
+
 sensor = Adafruit_DHT.DHT11
 
 pin = 23
@@ -8,7 +13,9 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-	if humidity is not None and temperature is not None:	
+	if humidity is not None and temperature is not None:
+		GPIO.output(38, GPIO.LOW)
+		print('Relay 1 is ON')
 		print temperature, humidity
 	else:
 		print('Failed to get reading. Try again!')
