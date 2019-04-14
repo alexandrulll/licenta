24f3ba713d4741330fd4de1db4ec7598c9344b57
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-14 17:19:15 +0300

    Add files via upload

--- sketch_apr08a/sketch_apr08a.ino
+++ sketch_apr08a/sketch_apr08a.ino
@@ -1,43 +1,17 @@
-/////////////////////////////////////////////////////////////////////////////
-// Sharp GP2Y1014AU0F Dust Sensor Demo
-//
-// Board Connection:
-//   GP2Y1014    Arduino
-//   V-LED       Between R1 and C1
-//   LED-GND     C1 and GND
-//   LED         Pin 7
-//   S-GND       GND
-//   Vo          A5
-//   Vcc         5V
-//
-// Serial monitor setting:
-//   9600 baud
-/////////////////////////////////////////////////////////////////////////////
-
-// Choose program options.
-//#define PRINT_RAW_DATA
 #define USE_AVG
 
-// Arduino pin numbers.
 const int sharpLEDPin = 7;   // Arduino digital pin 7 connect to sensor LED.
 const int sharpVoPin = A5;   // Arduino analog pin 5 connect to sensor Vo.
 
-// For averaging last N raw voltage readings.
 #ifdef USE_AVG
 #define N 100
 static unsigned long VoRawTotal = 0;
 static int VoRawCount = 0;
-#endif // USE_AVG
+#endif
 
-// Set the typical output voltage in Volts when there is zero dust. 
 static float Voc = 0.6;
-
-// Use the typical sensitivity in units of V per 100ug/m3.
 const float K = 0.5;
   
-/////////////////////////////////////////////////////////////////////////////
-
-// Helper functions to print a data value to the serial monitor.
 void printValue(String text, unsigned int value, bool isLast = false) {
   Serial.print(text);
   Serial.print("=");
@@ -46,6 +20,7 @@ void printValue(String text, unsigned int value, bool isLast = false) {
     Serial.print(", ");
   }
 }
+
 void printFValue(String text, float value, String units, bool isLast = false) {
   Serial.print(text);
   Serial.print("=");
@@ -56,47 +31,29 @@ void printFValue(String text, float value, String units, bool isLast = false) {
   }
 }
 
-/////////////////////////////////////////////////////////////////////////////
-
-// Arduino setup function.
 void setup() {
-  // Set LED pin for output.
-  pinMode(sharpLEDPin, OUTPUT);
   
-  // Start the hardware serial port for the serial monitor.
+  pinMode(sharpLEDPin, OUTPUT);
   Serial.begin(9600);
   
-  // Wait two seconds for startup.
   delay(2000);
-  Serial.println("");
-  Serial.println("GP2Y1014AU0F Demo");
-  Serial.println("=================");
 }
 
-// Arduino main loop.
 void loop() {  
-  // Turn on the dust sensor LED by setting digital pin LOW.
-  digitalWrite(sharpLEDPin, LOW);
-
-  // Wait 0.28ms before taking a reading of the output voltage as per spec.
-  delayMicroseconds(280);
-
-  // Record the output voltage. This operation takes around 100 microseconds.
-  int VoRaw = analogRead(sharpVoPin);
   
-  // Turn the dust sensor LED off by setting digital pin HIGH.
+  digitalWrite(sharpLEDPin, LOW);
+  delayMicroseconds(280);
+  
+  int VoRaw = analogRead(sharpVoPin);
   digitalWrite(sharpLEDPin, HIGH);
-
-  // Wait for remainder of the 10ms cycle = 10000 - 280 - 100 microseconds.
+  
   delayMicroseconds(9620);
   
-  // Print raw voltage value (number from 0 to 1023).
   #ifdef PRINT_RAW_DATA
   printValue("VoRaw", VoRaw, true);
   Serial.println("");
-  #endif // PRINT_RAW_DATA
+  #endif
   
-  // Use averaging if needed.
   float Vo = VoRaw;
   #ifdef USE_AVG
   VoRawTotal += VoRaw;
@@ -108,20 +65,19 @@ void loop() {
   } else {
     return;
   }
-  #endif // USE_AVG
+  #endif
 
-  // Compute the output voltage in Volts.
   Vo = Vo / 1024.0 * 5.0;
   printFValue("Vo", Vo*1000.0, "mV");
 
-  // Convert to Dust Density in units of ug/m3.
   float dV = Vo - Voc;
   if ( dV < 0 ) {
     dV = 0;
     Voc = Vo;
   }
+  
   float dustDensity = dV / K * 100.0;
   printFValue("DustDensity", dustDensity, "ug/m3", true);
   Serial.println("");
   
-} // END PROGRAM
+}
